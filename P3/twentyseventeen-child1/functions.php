c20c2b6f0e9616ad5dbbc3c0ee602c742c07b1b9
al342129 <36187211+al342129@users.noreply.github.com>
2019-11-08 17:50:52 +0100

    ; de los eggs

--- P3/twentyseventeen-child1/functions.php
+++ P3/twentyseventeen-child1/functions.php
@@ -4,6 +4,6 @@ add_filter("the_content", "mfp_Fix_Text_Spacing");
 function mfp_Fix_Text_Spacing($the_Post)
 {
 $the_New_Post = str_replace("piruleta", " PIRULETA ", $the_Post);
-return $the_New_Post
+return $the_New_Post;
 }
 ?>
