ea889b27cb430b3efd6d3dbf154b956b86459da6
al342129 <36187211+al342129@users.noreply.github.com>
2019-11-15 20:22:17 +0100

    Update functions.php

--- P3/my_group_ej6/include/functions.php
+++ P3/my_group_ej6/include/functions.php
@@ -137,7 +137,8 @@ function MP_my_datos1()
                 foreach ($rows as $row) {
                     print "<tr>";
                     foreach ($row as $key => $val) {
-                        echo "<td>", $val, "</td>";
+                        //echo "<td>", $val, "</td>";
+                        <?php echo implode('</td><td>', $val); ?>
                         
                         
                     }
