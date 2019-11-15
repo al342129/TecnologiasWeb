8c1054d9ee246d5a7e6af1473ec35840e6d0657a
al342129 <36187211+al342129@users.noreply.github.com>
2019-11-15 20:16:01 +0100

    Update functions.php

--- P3/my_group_ej6/include/functions.php
+++ P3/my_group_ej6/include/functions.php
@@ -137,8 +137,7 @@ function MP_my_datos1()
                 foreach ($rows as $row) {
                     print "<tr>";
                     foreach ($row as $key => $val) {
-                        //echo "<td>", $val, "</td>";
-                        echo "<th>", $val, "</th>";
+                        echo "<td align='center'>", $val, "</td>";
                         
                     }
                     print "</tr>";
