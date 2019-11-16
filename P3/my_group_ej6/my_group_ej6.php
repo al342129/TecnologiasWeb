af4680e1c76a03263e836a04c0d61ecdbc6495b0
al342129 <36187211+al342129@users.noreply.github.com>
2019-11-16 15:51:43 +0100

    Update my_group_ej6.php

--- P3/my_group_ej6/my_group_ej6.php
+++ P3/my_group_ej6/my_group_ej6.php
@@ -2,24 +2,39 @@
 /*
 Plugin Name: my_group_ej6
 Description: Register group of persons.
-Author URI: lola L
-Author Email: dllido@uji.es
+Author URI: Rocío Paniagua
+Author Email: al342129@uji.es
 Version: 1.0
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
 
+//Per a afegir regles CSS només per al Plugin desencolupem el següent gantxo:
+function hook_css(){
+    ?>
+        <style>
+            .wp_head{ 
+                background-color:#F3E2A9;
+            }
+            h1, h2, h3, h4 {
+                font-family: 'Lobster', cursive;
+             }
+            
+        </style>
+     <?php
+    }
+    add_action('wp_head', 'hook_css');
+
+
+
 
 //Al activar el plugin quiero que se cree una tabla en la BD, que creará la función my_group_install.
-
-
 
 //Añado action hook, de forma que cuando se realice la acción de una petición a la URL: wp-admin/admin-post.php?action=my_datos 
 //se llame a mi controlador definido en la función my_datos definido en el fichero include/functions.php
 
 //Solo activado el hook para usuarios autentificados,  
-
 
 
 //La siguiente sentencia activaria la acción para todos los usuarios.
